f9d083079049f1701e5713397fafd82678e7de5e
Nicolas Pinto <pinto@mit.edu>
2011-09-24 03:36:38 -0400

    Cosmetic: remove trailing whitespaces

--- genson/__init__.py
+++ genson/__init__.py
@@ -4,15 +4,15 @@ from util import *
 import copy
 
 class JSONGenerator:
-        
+
     def __init__(self, genson_dict):
         self.genson_dict = genson_dict
-        
+
         self.generators = []
         self.find_generators(genson_dict)
-        
+
         self.first_run = True
-    
+
     def find_generators(self, d):
         vals = d.values()
         for v in vals:
@@ -20,35 +20,35 @@ class JSONGenerator:
                 self.generators.append(v)
             if isdict(v):
                 self.find_generators(v)
-    
+
     def __iter__(self):
         return self
-     
-     
+
+
     def advance_generator_stack(self, cursor=0):
-        
+
         if cursor >= len(self.generators):
             return False
-        
+
         if self.generators[cursor].advance():
             return True
         else:
             self.generators[cursor].reset()
             return self.advance_generator_stack(cursor+1)
-        
-        
+
+
     def next(self):
-        
+
         if self.first_run:
             self.first_run = False
             return resolve(self.genson_dict)
-            
+
         if self.advance_generator_stack():
             return resolve(self.genson_dict)
         else:
             self.first_run = True
             raise StopIteration()
-            
+
 
 def load(io):
     s = "\n".join(io.readlines())
