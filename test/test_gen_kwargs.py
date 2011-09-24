40fefe541f566235e23d27fdcdc7d1e38720e883
Nicolas Pinto <pinto@mit.edu>
2011-09-24 03:11:16 -0400

    BUG: expose gen_kwarg with underscore error

--- test/test_gen_kwargs.py
+++ test/test_gen_kwargs.py
@@ -3,7 +3,22 @@ import genson
 from os import path
 my_path = path.dirname(path.abspath(__file__))
 
-def test_gen_kwargs_tuple_bug():
-    test_fname = path.join(my_path, 'test_gen_kwargs_tuple_bug.gson')
-    with open(test_fname, 'r') as fin:
-        genson.load(fin)
+def test_multi_gen_kwargs():
+    gson = \
+    """
+    {
+        "multi_gen_kwargs_bug" : { "nested": gaussian(0, 1, draws=1, rseed=42) }
+    }
+    """
+    genson.loads(gson)
+
+
+def test_gen_kwargs_with_underscore():
+    gson = \
+    """
+    {
+        "gen_kwarg_with_underscore" : { "nested": gaussian(0, 1, draws=1, random_seed=42) }
+    }
+    """
+    genson.loads(gson)
+
