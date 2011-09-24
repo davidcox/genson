b46e44ca58631f0530541e8ab081338570edcdd2
Nicolas Pinto <pinto@mit.edu>
2011-09-24 03:34:50 -0400

    FIX: test.gson path

--- test/test.py
+++ test/test.py
@@ -1,24 +1,29 @@
 import genson
 import time
 
+from os import path
+my_path = path.dirname(path.abspath(__file__))
+
+
 def parse_and_report_error(doc):
     try:
         son_iterator = genson.loads(doc)
     except Exception as e:
         print("Caught exception: %s" % e)
         return
-    
+
     print("Did not catch exception")
 
-if __name__ == "__main__":
-    
+
+def test_main():
+
     testdata = """
        {
            "test0": 4,
            "test1" : <0,1,2>,
            "test2" : { "nested": gaussian(0,1,draws=1) },
            "test3" : <"a", "b", uniform(0,1)>,
-           ("test4", "test5") : (0, 1), 
+           ("test4", "test5") : (0, 1),
            ("test6", "test7") : 1,
            ("test8","test9") : <("d", "e"), ("f", "g")>,
            "testA": {"another_nested" : root.test5,
@@ -39,12 +44,12 @@ if __name__ == "__main__":
            "test0": 4,
            "test1" : <0,1,2>,
            "test3" : <"a", "b", uniform(0,1)>,
-           ("test4", "test5") : (0, 1), 
+           ("test4", "test5") : (0, 1),
            ("test6", "test7") : 1,
            ("test8","test9") : <("d", "e"), ("f", "g")>
         }
     """
-    
+
     test_broken1 = """
         {
             // no key
@@ -52,7 +57,7 @@ if __name__ == "__main__":
             "test1" : 5
         }
     """
-    
+
     test_broken2 = """
         {
             // no value
@@ -60,7 +65,7 @@ if __name__ == "__main__":
             "test1" : 5
         }
     """
-    
+
     test_broken3 = """
         {
             // no comma
@@ -68,29 +73,27 @@ if __name__ == "__main__":
             "test1" : 5
         }
     """
-      
+
     tic = time.time()
     son_iterator1 = genson.loads(testdata2)
     toc = time.time() - tic
     print("Simple example parse time: %s" % toc)
-           
+
     tic = time.time()
     son_iterator2 = genson.loads(testdata)
     toc = time.time() - tic
     print("Complex example parse time: %s" % toc)
-    
-    
+
     for d in son_iterator2:
         print d
     print
 
     print "Again, but from a formatted file:"
-    with open('test.gson') as f:
+    with open(path.join(my_path, 'test.gson')) as f:
         son_file_iterator = genson.load(f)
 
     for d in son_file_iterator:
         print d
-
 
     parse_and_report_error(test_broken1)
     parse_and_report_error(test_broken2)
