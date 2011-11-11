4403144c02a25eacb2060477fbe1803fc2892d1d
Daniel Yamins <dyamins@gmail.com>
2011-11-11 08:41:02 -0500

    pep8 and index references

--- genson/references.py
+++ genson/references.py
@@ -1,13 +1,13 @@
 from internal_ops import GenSONOperand
-from util import resolve, isdict, genson_dumps
+from util import resolve, isdict, isindexable, genson_dumps
 import copy
+
 
 def resolve_scoped_reference(ref, context):
     """ Given a reference scope in list format (e.g. ['this', 'key1', 'key2']),
         and a context stack (list of nested dictionary references), resolve
         the reference and return the value
     """
-
 
     if len(context) == 0:
         # TODO: better
@@ -16,24 +16,28 @@ def resolve_scoped_reference(ref, context):
     # pop an element off of the scope list
     element_to_resolve = ref.pop(0)
 
-    if( element_to_resolve == 'root'):
+    if (element_to_resolve == 'root'):
         return resolve_scoped_reference(ref, [context[0]])
 
-    if( element_to_resolve == 'parent'):
+    if (element_to_resolve == 'parent'):
         return resolve_scoped_reference(ref, context[0:-1])
 
-    if( element_to_resolve == 'this'):
+    if (element_to_resolve == 'this'):
         return resolve_scoped_reference(ref, context)
 
     current_context = context[-1]
 
-
-    if not isdict(current_context):
+    if isdict(current_context):
+        if not element_to_resolve in current_context:
+            # TODO: make better
+            raise Exception("Unknown key: %s" % element_to_resolve)
+    elif isindexable(current_context):
+        element_to_resolve = int(element_to_resolve)
+        if not 0 <= element_to_resolve < len(current_context):
+            raise Exception("Reference index out of range: %d" %
+                                                  element_to_resolve)
+    else:
         raise Exception("Invalid reference")
-
-    if not element_to_resolve in current_context:
-        # TODO: make better
-        raise Exception("Unknown key: %s" % element_to_resolve)
 
     resolved_element = current_context[element_to_resolve]
 
@@ -49,11 +53,12 @@ class ScopedReference (GenSONOperand):
         self.scope_list = scope_list
 
     def __genson_eval__(self, context):
-        return resolve_scoped_reference( copy.deepcopy(self.scope_list),
-                                         copy.copy(context) )
-    
+        return resolve_scoped_reference(copy.deepcopy(self.scope_list),
+                                         copy.copy(context))
+
     def __genson_repr__(self, pretty_print=False, depth=0):
         return ".".join(self.scope_list)
+
 
 def ref(ref_str):
     "A helper to convert a genson ref string into a SopeReference object"
