acaf9980766a3ded7894dec32df09e9f73f626bf
David Cox <david.daniel.cox@gmail.com>
2012-01-31 16:50:04 -0500

    Add tests for genson expression

---
+++ test/test_expressions.py
@@ -0,0 +1,16 @@
+import genson
+
+
+def test_unary():
+
+    g = genson.loads('{ "p" : sin(-1) } ')
+    assert( g['p'].args[0] == -1)
+
+    g = genson.loads('{ "p" : sin(+1) } ')
+    assert( g['p'].args[0] == 1)
+
+def test_binary():
+
+    g = genson.loads('{ "p" : gaussian(1+1,1) } ')
+    assert( g['p'].mean == 2)
+    assert( g['p'].stdev == 1)
