0c868909d56273052c90aa6277834593d39260c7
James Bergstra <james.bergstra@gmail.com>
2012-02-02 11:13:48 -0500

    adding tests

---
+++ test/test_lazy.py
@@ -0,0 +1,42 @@
+from collections import OrderedDict
+from genson import lazy
+from genson import JSONFunction
+from genson import ref
+
+
+@lazy
+def foo(a, b=None):
+    return 'first_arg', 'second_arg', a, b
+
+def test_lazy_callable():
+    assert foo(1) == ('first_arg', 'second_arg', 1, None)
+    assert foo(1, 3) == ('first_arg', 'second_arg', 1, 3)
+    assert foo(1, b=3) == ('first_arg', 'second_arg', 1, 3)
+    assert foo(b=3, a=1) == ('first_arg', 'second_arg', 1, 3)
+
+def test_lazy_getitem_calldoc_args_kwargs():
+    # -- construct a program document directly
+    #    without passing through the parser
+    prog = OrderedDict()
+    prog['args'] = 'from_calldoc'
+    prog['kwargs'] = 'from_calldoc'
+    prog['test0'] = 4
+    prog['test1'] = foo.lazy(1)
+    prog['test2'] = ref('test1')[0]
+    prog['test3'] = ref('test1')[1]
+    prog['test4'] = ref('args')[0]
+    prog['test5'] = ref('kwargs')['aaa']
+
+    wanted = dict(
+            test0=4,
+            test1=('first_arg', 'second_arg', 1, None),
+            test2='first_arg',
+            test3='second_arg',
+            test4=33,
+            test5='hello')
+
+    print prog
+    f = JSONFunction(prog)
+    result = dict(f(33, aaa='hello'))
+    print result
+    assert result == wanted
